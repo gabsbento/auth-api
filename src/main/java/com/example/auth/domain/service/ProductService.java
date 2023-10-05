d288f0196642333617800006ffff724b3f5a857f
Gabriel Bento <gab.silva.bento@gmail.com>
2023-10-05 18:12:37 -0300

    Feat: add product service

---
+++ src/main/java/com/example/auth/domain/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.example.auth.domain.service;
+
+import com.example.auth.domain.entity.Product;
+import com.example.auth.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    public Optional<Product> findById(String id) {
+        return productRepository.findById(id);
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+}
